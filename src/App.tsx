1396c7b9f9e7b4ba5703ba312d3fe9c01472e6aa
1jack80 <work.davidmainoo@gmail.com>
2023-03-12 21:23:58 +0000

    update user name and repo name

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+	return (
+		<div className="App">
+			<h2>404 not found</h2>
+			<div className="container">
+				<picture>
+					<img
+						alt="scarecrow"
+						src="./Scarecrow.png"
+					/>
+				</picture>
 
-    return (
-        <div className="App">
-            <h2>404 not found</h2>
-            <div className='container'>
-                <picture>
-                    <img alt="scarecrow" src="./Scarecrow.png" />
-                </picture>
-
-                <article className='article'>
-
-                    <h1 className='article_title'>I have bad news for you</h1>
-                    <p className='article_paragraph'>The page you were looking for might be removed or is temporarily unavailable</p>
-                    <button className='article_button'> Back to homepage </button>
-
-                </article>
-            </div>
-            <footer>created by <a href='github.com/David-Main'>David-Main</a> - devChallenges.io </footer>
-        </div>
-    )
+				<article className="article">
+					<h1 className="article_title">I have bad news for you</h1>
+					<p className="article_paragraph">
+						The page you were looking for might be removed or is temporarily
+						unavailable
+					</p>
+					<button className="article_button"> Back to homepage </button>
+				</article>
+			</div>
+			<footer>
+				created by <a href="github.com/1jack80">1jack80</a> - devChallenges.io{" "}
+			</footer>
+		</div>
+	);
 }
 
-export default App
+export default App;
